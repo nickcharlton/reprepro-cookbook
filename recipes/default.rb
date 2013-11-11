5020a1cef6c9552dfe7090b6953ea5d7a68fb431
Nick Charlton <hello@nickcharlton.net>
2013-11-11 10:20:49 +0000

    Sets the dependencies to install; fleshes out the structure.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,19 @@
 # 
 # MIT Licensed.
 #
+
+# install the reprepro dependencies
+%w{apt-utils dpkg-dev reprepro debian-keyring devscripts dput}.each do |pkg|
+  package pkg
+end
+
+# create a user to hold the packages and key
+
+# build the reprepro directory structure
+
+# import the GPG key
+
+# populate the reprepro configuration
+
+# configure nginx to host it
+
