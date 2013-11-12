eacaf888a1bacce848d7fc06926de2f90c0f8029
Nick Charlton <hello@nickcharlton.net>
2013-11-12 13:52:22 +0000

    Implements the gpg key handling.

--- metadata.rb
+++ metadata.rb
@@ -10,5 +10,6 @@ version          '0.1.0'
 supports 'debian'
 supports 'ubuntu'
 
+depends 'chef-vault'
 depends 'nginx'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,20 @@
 # MIT Licensed.
 #
 
+include_recipe 'chef-vault'
 include_recipe 'nginx'
+
+# extract further configuration from the vault
+begin
+  if Chef::Config[:solo]
+    apt_repo = nil
+  else
+    apt_repo = chef_vault_item('reprepro', 'main')
+  end
+rescue ChefVault::Exceptions::KeysNotFound, OpenSSL::PKey::RSAError
+  Chef::Log.warn('Could\'t find or decrypt the specified vault. The GPG components '\
+    'will not be configured. This may not be what you want.')
+end
 
 # install the reprepro dependencies
 %w{apt-utils dpkg-dev reprepro debian-keyring devscripts dput}.each do |pkg|
@@ -38,7 +51,40 @@ node['reprepro']['distributions'].keys.each do |dist|
   end
 end
 
-# import the GPG key
+# don't try and configure the gpg key unless we can access it
+if apt_repo
+  # import the private key
+  # see: https://tickets.opscode.com/browse/CHEF-2288
+  # there's also another problem with passing the key along a pipe writing it
+  # to a file fixes this
+  bash 'import packaging key' do
+    code <<-EOH
+    sudo -u packages HOME=/home/packages gpg --list-secret-keys --fingerprint \
+'#{apt_repo['gpg']['email']}' | egrep -qx '.*Key fingerprint = \
+#{apt_repo['gpg']['fingerprint']}'
+
+    if [ $? -ne 1 ]; then 
+      sudo -u packages /bin/echo -e "#{apt_repo['gpg']['public']}" > /home/packages/packages_pub.gpg
+      sudo -u packages /bin/echo -e "#{apt_repo['gpg']['private']}" > /home/packages/packages_sec.gpg
+
+      sudo -u packages HOME=/home/packages gpg --import /home/packages/packages_pub.gpg
+      sudo -u packages HOME=/home/packages gpg --allow-secret-key-import --import /home/packages/packages_sec.gpg
+    fi
+    EOH
+  end
+
+  # place the public key …public
+  template "#{node['reprepro']['packages_path']}/gpg_key.pub" do
+    source 'gpg_key.erb'
+    owner 'packages'
+    group 'packages'
+    mode '0644'
+
+    variables(
+      :key => apt_repo['gpg']['public']
+    )
+  end
+end
 
 # populate the reprepro configuration
 node['reprepro']['distributions'].keys.each do |dist|
